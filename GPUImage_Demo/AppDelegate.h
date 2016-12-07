3e83a892ddc004bcec9c6bbbe186a40213369c05
zhangzhaopds <zhangzhaopds@foxmail.com>
2016-12-07 08:57:46 +0800

    Initial Commit

---
+++ GPUImage_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPUImage_Demo
+//
+//  Created by 张昭 on 07/12/2016.
+//  Copyright © 2016 张昭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPUImage_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPUImage_Demo
+//
+//  Created by 张昭 on 07/12/2016.
+//  Copyright © 2016 张昭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
